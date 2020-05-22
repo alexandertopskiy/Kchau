4d9f38836ab1375cf3038436b3711831efd4a27a
Alexander Loshakov <alevas556@icloud.com>
2020-05-22 21:30:14 +0300

    Работает и ладно

--- client/app.js
+++ client/app.js
@@ -1,6 +1,4 @@
 var organizeByTags = function (toDoObjects) { 
-	
-	console.log("organizeByTags called");
 	// создание пустого массива для тегов
 	var tags = [];
 	// перебираем все задачи toDos 
@@ -13,8 +11,6 @@ var organizeByTags = function (toDoObjects) {
 			}
 		});
 	}); 
-	console.log(tags);
-
 	var tagObjects = tags.map(function (tag) {
 		// здесь мы находим все задачи,
 		// содержащие этот тег
@@ -29,91 +25,89 @@ var organizeByTags = function (toDoObjects) {
 		// мы связываем каждый тег с объектом, который // содержит название тега и массив
 		return { "name": tag, "toDos": toDosWithTag };
 	});
-	console.log(tagObjects);
 	return tagObjects;
 };
-
 var main = function (toDoObjects) {
 	"use strict";
-	// как main имеет доступ к списку задач!
 	var toDos = toDoObjects.map(function (toDo) {
-		// просто возвращаем описание этой задачи
+		// просто возвращаем описание
+		// этой задачи
 		return toDo.description;
 	});
-	// сейчас весь старый код должен работать как раньше!
-	$("document").ready( function(){
-
-	$(".tabs a span").toArray().forEach(function (element) { 
-		// создаем обработчик щелчков для этого элемента 
-		$(element).on("click", function () {
+	
+	$(".tabs a span").toArray().forEach(function (element) {
+		// создаем обработчик щелчков для этого элемента
+		$(element).on("click", function() {
 			// поскольку мы используем версию элемента jQuery,
-			// нужно создать временную переменную,
+			// нужно создать временную переменную, 
 			// чтобы избежать постоянного обновления
 			var $element = $(element);
-			$(".tabs a span").removeClass("active"); 
-			$(element).addClass("active");
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
-
-			if ($element.parent().is(":nth-child(1)")) { 
-				for (var i = toDos.length-1; i > -1; i--) { 
+			if ($element.parent().is(":nth-child(1)")) {
+				for (var i = toDos.length-1; i > -1; i--) {
 					$(".content").append($("<li>").text(toDos[i]));
 				}
-			} 
-			else if ($element.parent().is(":nth-child(2)")) { 
-				toDos.forEach(function (todo) { 
+			} else if ($element.parent().is(":nth-child(2)")) {
+				toDos.forEach(function (todo) {
 					$(".content").append($("<li>").text(todo));
 				});
-			} 
-			else if ($element.parent().is(":nth-child(3)")) { 
-				// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ 
-				console.log("Щелчок на вкладке Теги");
-				// organizeByTag(toDoObjects);
+			} else if ($element.parent().is(":nth-child(3)")) {
+				// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
 				var organizedByTag = organizeByTags(toDoObjects);
-				
-				 organizedByTag.forEach(function (tag) { 
-					var $tagName = $("<h3>").text(tag.name), 
-					$content = $("<ul>"); 
-					tag.toDos.forEach(function (description) { 
-						var $li = $("<li>").text(description); 
+				organizedByTag.forEach(function (tag) {
+					var $tagName = $("<h3>").text(tag.name),
+					$content = $("<ul>");
+					tag.toDos.forEach(function (description) {
+						var $li = $("<li>").text(description);
 						$content.append($li);
 					});
-					$("main .content").append($tagName); 
+					$("main .content").append($tagName);
 					$("main .content").append($content);
 				});
-
-
-			} 
-			else if ($element.parent().is(":nth-child(4)")) { 
+			} else if ($element.parent().is(":nth-child(4)")) {
 				var $input = $("<input>").addClass("description"), 
-					$inputLabel = $("<p>").text("Новая задача: "),
-					$tagInput = $("<input>").addClass("tags"),
-					$tagLabel = $("<p>").text("Тэги: "),
-					$button = $("<button>").text("+");
-				$button.on("click", function () {
+				$textInput = $("<p>").text("Введите новую задачу: "),
+				$tagInput = $("<input>").addClass("tags"),
+				$tagLabel = $("<p>").text("Тэги: "),
+				$button = $("<button>").text("+");
+				$("main .content").append($textInput).append($input).append($tagLabel).append($tagInput).append($button); 
+				function btnfunc() {
 					var description = $input.val(),
-					// разделение в соответствии с запятыми
-					tags = $tagInput.val().split(","); 
-					toDoObjects.push({"description":description, "tags":tags}); 
-					// обновление toDos
-					toDos = toDoObjects.map(function (toDo) {
-						return toDo.description;
+						tags = $tagInput.val().split(","),
+						// создаем новый элемент списка задач
+						newToDo = {"description":description, "tags":tags};
+					$.post("todos", newToDo, function(result) {
+						console.log(result);
+						// нужно отправить новый объект на клиент
+						// после получения ответа сервера
+						toDoObjects.push(newToDo);
+						// обновляем toDos
+						toDos = toDoObjects.map(function (toDo) {
+							return toDo.description;
+						});
+						$input.val("");
+						$tagInput.val("");
+						$(".tabs a:first-child span").trigger("click");
 					});
-					$input.val("");
-					$tagInput.val("");
+				}
+				$button.on("click", function() {
+					btnfunc();
 				});
-				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button); 
+				$('.tags').on('keydown',function(e){
+					if (e.which === 13) {
+						btnfunc();
+					}
+				});
 			}
 			return false;
-		})
-	})
+		});
+	});
 	$(".tabs a:first-child span").trigger("click");
-
-	})
-};	
-
-$(document).ready(function () {
+};
+$(document).ready(function() {
 	$.getJSON("todos.json", function (toDoObjects) {
-	// вызов функции main с аргументом в виде объекта toDoObjects 
 		main(toDoObjects);
 	});
 });
--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-var express = require('express'), 
-http = require("http"),
-app = express(),
-toDos = [
-{
-"description" : "Купить продукты",
-"tags" : [ "шопинг", "рутина" ]
-},
-{
-"description" : "Сделать несколько новых задач",
-"tags" : [ "писательство", "работа" ]
-},
-{
-"description" : "Подготовиться к лекции в понедельник", 
-"tags" : [ "работа", "преподавание" ]
-},
-{
-"description" : "Ответить на электронные письма", 
-"tags" : [ "работа" ]
-},
-{
-"description" : "Вывести Грейси на прогулку в парк", 
-"tags" : [ "рутина", "питомцы" ]
-},
-{
-"description" : "Закончить писать книгу", 
-"tags" : [ "писательство", "работа" ]
-}
-];
-app.use(express.static(__dirname + "/client")); 
+var express = require("express"),
+	http = require("http"),
+	app = express(),
+	toDos = [
+		// настраиваем список задач копированием
+		// содержимого из файла todos.OLD.json
+	];
+app.use(express.static(__dirname + "/client"));
 http.createServer(app).listen(3000);
-// этот маршрут замещает наш файл
-// todos.json в примере из части 5 
-app.get("/todos.json", function (req, res) { 
-res.json(toDos);
+// Этот маршрут замещает наш файл
+// todos.json в примере из части 5
+app.get("/todos.json", function (req, res) {
+	res.json(toDos);
 });
-
-app.post("/todos", function (req, res) { 
-console.log("Данные были отправлены на сервер!"); 
-// простой объект отправлен обратно 
-res.json({"message":"Вы размещаетесь на сервере!"}); 
+// командуем Express принять поступающие
+// объекты JSON
+app.use(express.urlencoded({ extended: true }));
+app.post("/todos", function (req, res) {
+	// сейчас объект сохраняется в req.body
+	var newToDo = req.body;
+	console.log(newToDo);
+	toDos.push(newToDo);
+	// отправляем простой объект
+	res.json({"message":"Вы размещаетеся на сервере!"});
 });
