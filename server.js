c5b1368ebef758cd13989fa38d08cc05cb90c869
Alexander Loshakov <alevas556@icloud.com>
2020-05-22 19:51:01 +0300

    Успешный запуск сервера в виртуальной машине

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express'), 
+http = require("http"),
+app = express(),
+toDos = [
+{
+"description" : "Купить продукты",
+"tags" : [ "шопинг", "рутина" ]
+},
+{
+"description" : "Сделать несколько новых задач",
+"tags" : [ "писательство", "работа" ]
+},
+{
+"description" : "Подготовиться к лекции в понедельник", 
+"tags" : [ "работа", "преподавание" ]
+},
+{
+"description" : "Ответить на электронные письма", 
+"tags" : [ "работа" ]
+},
+{
+"description" : "Вывести Грейси на прогулку в парк", 
+"tags" : [ "рутина", "питомцы" ]
+},
+{
+"description" : "Закончить писать книгу", 
+"tags" : [ "писательство", "работа" ]
+}
+];
+app.use(express.static(__dirname + "/client")); 
+http.createServer(app).listen(3000);
+// этот маршрут замещает наш файл
+// todos.json в примере из части 5 
+app.get("/todos.json", function (req, res) { 
+res.json(toDos);
+});
