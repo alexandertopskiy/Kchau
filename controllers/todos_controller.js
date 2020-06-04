c197780e801b55e2ffb3da08fe612d96f67fa8f8
Alexander Loshakov <alevas556@icloud.com>
2020-06-04 20:27:08 +0300

    Настройка маршрутов todos

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -4,9 +4,30 @@ var ToDo = require("../models/todo.js"),
 	ToDosController = {};
 
 ToDosController.index = function (req, res) { 
-	ToDo.find({}, function (err, toDos) { 
-		res.json(toDos);
-	});
+	var username = req.params.username || null,
+		respondWithToDos;
+	respondWithToDos = function (query) {
+		ToDo.find(query, function (err, toDos) {
+			if (err !== null) {
+				res.json(500, err);
+			} else {
+				res.status(200).json(toDos);
+			}
+		});
+	};
+	if (username !== null) {
+		User.find({"username": username}, function (err, result) {
+			if (err !== null) {
+				res.json(500, err);
+			} else if (result.length === 0) {
+				res.status(404).json("result_length" : 0);
+			} else {
+				respondWithToDos({});
+			}
+		});
+	} else {
+		res.status(404).json({"username": username});
+	}
 };
 
 ToDosController.create = function (req, res) {
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var express = require("express"),
 	http = require("http"),
 	mongoose = require("mongoose"),
-	// импорт представления ToDoController
 	ToDosController = require("./controllers/todos_controller.js"),
 	app = express(); 
 
-// Это модель Mongoose для задач
-var ToDoSchema = mongoose.Schema({
-	description: String,
-	tags: [ String ]
-});
-// var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
 
-app.use(express.static(__dirname + "/client"));
+app.use('/',express.static(__dirname + "/client"));
+app.use('/user/:username',express.static(__dirname + "/client"));
 
-app.get("/todos.json", ToDosController.index);
-// базовые маршруты CRUD 
-app.get("/todos/:id", ToDosController.show); 
-app.post("/todos", ToDosController.create);
-
-// командуем Express принять поступающие
-// объекты JSON
+// командуем Express принять поступающие объекты JSON
 app.use(express.urlencoded({ extended: true }));
 
 // подключаемся к хранилищу данных Amazeriffic в Mongo
@@ -29,11 +17,21 @@ mongoose.connect('mongodb://localhost/amazeriffic', {
 		useNewUrlParser: true,
 		useCreateIndex: true,
 		useUnifiedTopology: true 
-	}).then(res => {
-		console.log("DB Connected!")
-	}).catch(err => {
-		console.log(Error, err.message);
-	});
+}).then(res => {
+	console.log("DB Connected!")
+}).catch(err => {
+	console.log(Error, err.message);
+});
+
+app.get("/todos.json", ToDosController.index);
+app.get("/todos/:id", ToDosController.show); 
+app.post("/todos", ToDosController.create);
+
+app.get("/users/:username/todos.json", ToDosController.index);
+app.post("/users/:username/todos", ToDosController.create);
+app.put("/users/:username/todos/:id", ToDosController.update);
+app.delete("/users/:username/todos/:id", ToDosController.destroy);
+
 
 
 
