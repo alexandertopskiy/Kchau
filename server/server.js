dc4e666f68ae4ad4bc6e6f6720a6a72da95497e8
Alexander Loshakov <alevas556@icloud.com>
2023-02-01 14:21:40 +0300

    Рефакторинг server.js

--- server/server.js
+++ server/server.js
@@ -7,42 +7,41 @@ var express = require('express'),
 
 http.createServer(app).listen(3000);
 
-console.log(__dirname);
-console.log(__dirname + '../client');
 app.use('/', express.static(__dirname + '/../client'));
 app.use('/user/:username', express.static(__dirname + '/../client'));
 
 // командуем Express принять поступающие объекты JSON
 app.use(express.urlencoded({ extended: true }));
 
-// подключаемся к хранилищу данных Amazeriffic в Mongo
+// Подключение к коллекции ParkingForMen в MongoDB
 mongoose
-    .connect('mongodb://localhost/amazeriffic', {
+    .connect('mongodb://localhost/ParkingForMen', {
         useNewUrlParser: true,
-
         useUnifiedTopology: true
     })
-    .then(res => {
+    .then(() => {
         console.log('DB Connected!');
     })
     .catch(err => {
-        console.log('error!!!');
         console.log(Error, err.message);
     });
 
+// Запросы для Пользователей
+app.get('/users.json', UsersController.index);
+app.post('/users', UsersController.create);
+app.get('/users/:username', UsersController.show);
+app.put('/users/:username', UsersController.update);
+app.delete('/users/:username', UsersController.destroy);
+
+// Запросы для Записей (Квитанций)
 app.get('/receipts.json', ReceiptController.index);
 app.get('/receipts/:id', ReceiptController.show);
 app.post('/receipts', ReceiptController.create);
 app.put('/receipts/:id', ReceiptController.update);
 app.delete('/receipts/:id', ReceiptController.destroy);
 
+// Запросы для Записей (Квитанций) конкретного пользователя
 app.get('/users/:username/receipts.json', ReceiptController.index);
 app.post('/users/:username/receipts', ReceiptController.create);
 app.put('/users/:username/receipts/:id', ReceiptController.update);
 app.delete('/users/:username/receipts/:id', ReceiptController.destroy);
-
-app.get('/users.json', UsersController.index);
-app.post('/users', UsersController.create);
-app.get('/users/:username', UsersController.show);
-app.put('/users/:username', UsersController.update);
-app.delete('/users/:username', UsersController.destroy);
